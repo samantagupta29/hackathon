878868fbd0a5ff44f544901e396462f09b876cdd
Samanta Gupta <samanta@internshala.com>
2024-09-18 22:05:59 +0530

    individual folder created

---
+++ ML/test.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/status', methods=['GET'])
+def status():
+    response = {
+        'status': 'success',
+        'message': 'This is a dummy message.'
+    }
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
