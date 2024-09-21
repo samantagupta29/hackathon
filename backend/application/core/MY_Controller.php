4e7223c0667c658dcb84d1992060d1c5149279ee
root <root@ip-172-31-37-173.ap-south-1.compute.internal>
2024-09-21 04:14:30 +0000

    header added

--- backend/application/core/MY_Controller.php
+++ backend/application/core/MY_Controller.php
@@ -5,6 +5,8 @@ class MY_Controller extends CI_Controller {
 	public $user_id;
 
 	public function __construct() {
+		header("Access-Control-Allow-Origin: *");
+
 		parent::__construct();
 		$this->user_id = 1;
 	}
