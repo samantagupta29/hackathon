b5d8c879ff78c167f3033d35d73d8ea4ccf5266b
Naaz <65398335+naazkakria@users.noreply.github.com>
2024-09-21 09:21:45 +0530

    request handled

--- front-end/src/component/FilterPanel.jsx
+++ front-end/src/component/FilterPanel.jsx
@@ -25,7 +25,7 @@ const FilterPanel = () => {
 
   const handleLoading = async () => {
     try {
-      const response = await axios.get('user_preferences/apply_filters_and_get_recipes');
+      const response = await axios.get('http://13.200.223.248/user_preferences/apply_filters_and_get_recipes');
       // const response2 = await axios.get('/api/user_preferences/get_filters/apply_filters_and_get_recipes', payload);
       setRecipes(response.data.recipes)
       console.log('Success: default recipes are loaded', response.data);
@@ -78,7 +78,9 @@ const FilterPanel = () => {
     };
 
     try {
-      const response = await axios.get('user_preferences/apply_filters_and_get_recipes', payload);
+      //const response = await axios.get('user_preferences/apply_filters_and_get_recipes', payload);
+      const response = await axios.get('http://13.200.223.248/user_preferences/apply_filters_and_get_recipes', payload);
+      setRecipes(response.data.recipes)
       setRecipes(response.data.recipes)
       console.log('Success:', response.data);
       // Handle successful response (e.g., reset filters, show a success message)
