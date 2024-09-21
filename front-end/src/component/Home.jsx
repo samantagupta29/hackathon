2f101d96cd1c32054f215cd639de8263e65496e3
root <root@ip-172-31-37-173.ap-south-1.compute.internal>
2024-09-21 11:48:16 +0000

    changes

--- front-end/src/component/Home.jsx
+++ front-end/src/component/Home.jsx
@@ -24,15 +24,23 @@ function Home() {
       {isFilterOpen && <FilterPanel />}
 
       <div className='mt-4'>
+        
+        {/* Button to go to the "Generate Recipe" page */}
+        <Link 
+          className="absolute right-10 top-50 bg-red-600 text-white border-none rounded-lg px-4 py-2 cursor-pointer text-lg" 
+          to="/generate_receipe"
+        >
+          Generate recipe
+        </Link>
 
-        {/* Button to open the filter panel */}
-        {/* <button 
-          className="absolute right-10 top-50 bg-red-600 text-white border-none rounded-lg px-4 py-2 cursor-pointer text-lg"
-          onClick={toggleFilterPanel}
-        > */}
-          <Link className="absolute right-10 top-50 bg-red-600 text-white border-none rounded-lg px-4 py-2 cursor-pointer text-lg" to="/generate_receipe"> Generate recipe</Link>
-         
-        {/* </button> */}
+        {/* Button to go to the "Currency" page */}
+        <Link 
+          className="absolute left-10 top-50 bg-blue-600 text-white border-none rounded-lg px-4 py-2 cursor-pointer text-lg" 
+          to="/currency"
+        >
+         Linious Currency 
+        </Link>
+
       </div> 
     </div>
   );
